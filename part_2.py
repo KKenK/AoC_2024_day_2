1683d9f3c76be0d01c9b6e425693256a2a660e5b
Kyle <kylekenrick55@gmail.com>
2025-01-26 02:14:22 +0000

    part_2: working solution committed.

--- part_2.py
+++ part_2.py
@@ -1,9 +1,10 @@
 from input_parser import InputParser
+import copy
 
 class Report():
     def __init__(self, levels):
         self.levels = levels
-        self.state = None
+        self.is_safe = "Indeterminate"
 
 def make_reports(input):
     
@@ -13,71 +14,65 @@ def make_reports(input):
 
 def evaluate_reports_state(reports):
 
-    for report in range(len(reports)):
-        reports[report].state = check_if_report_is_safe(reports[report])
-    
+    for report_index in range(len(reports)):
+
+        levels = reports[report_index].levels    
+        
+        report_is_ascending = is_ascending(levels)
+
+        if not report_is_ascending:          
+            levels = [x * -1 for x in levels]      
+   
+        reports[report_index].is_safe = check_if_report_is_safe(levels)
+
+        if not reports[report_index].is_safe:
+
+            for i in range(len(levels) - 1):
+
+                reports[report_index].is_safe = check_if_report_is_safe(levels[:i] + levels[i + 1:])
+
+                if reports[report_index].is_safe:
+                    break
+            
+            if not reports[report_index].is_safe:
+                reports[report_index].is_safe = check_if_report_is_safe(levels[:-1])
+            
     return reports
 
-def check_if_report_is_safe(report):
-
-    report_indexs = len(report.levels) - 1
-
-    levels = report.levels
-
-    i = 0
-
-    bad_level_counter = 0
-
-    if levels[0] < levels[1]:
-        
-        while i < report_indexs:
-
-            report_difference = levels[i] - levels[i + 1]
-
-            if report_difference not in [-1, -2, -3]:
-                
-                bad_level_counter += 1
-
-                if bad_level_counter > 1:
-                    return "Unsafe"
-
-                levels.pop(i + 1)
-          
-                report_indexs -= 1
-                                                     
-                continue
-
-            i += 1
-        
-    elif levels[0] > levels[1]:
-
-       while i < report_indexs:
-
-            report_difference = levels[i] - levels[i + 1]
-
-            if report_difference not in [1, 2, 3]:
-                
-                bad_level_counter += 1
-
-                if bad_level_counter > 1:
-                    return "Unsafe"
-
-                levels.pop(i + 1)
-
-                report_indexs -= 1
-
-                continue
-            
-            i += 1
-                                   
-    else:
-        return "Unsafe"
+def is_ascending(levels):
     
-    return "Safe"
+    direction = 0
+
+    for i in range(1, len(levels)):
+
+        difference = levels[i] - levels[i - 1]
+
+        if difference > 0:
+            direction += 1
+        
+        elif difference < 0:
+            direction -= 1
+        
+    if direction > 0:
+        return True
+    
+    return False
+
+def check_if_report_is_safe(levels):
+        
+    for i in range(1, len(levels)):
+
+        report_difference = levels[i] - levels[i - 1]
+
+        if report_difference not in [1, 2, 3]:
+            
+            return False
+    
+    return True
 
 if __name__ == "__main__":
 
-    input_parser = InputParser(r"C:\Users\kylek\Documents\code\Advent_of_code\2024\Day_2\test.txt")
+    input_parser = InputParser(r"C:\Users\kylek\Documents\code\Advent_of_code\2024\Day_2\input.txt")
 
     reports = make_reports(input_parser.parsed_input)
 
@@ -87,7 +82,7 @@ if __name__ == "__main__":
     safe_report_counter = 0
 
     for report in reports:
-        if report.state == "Safe":
+        if report.is_safe == True:
             safe_report_counter += 1
     
     print(safe_report_counter)
